b8d1f30f3be4b0b206608caf0d2b39970ee67b89
carl <carl.stroemstedt@open-circle.ch>
2023-06-30 15:16:33 +0200

    fix: added back registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:registrations]
+  devise_for :users
   resources :dreams
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
