35f93fbaa5070abdfa3c1f7da53c196d1f712702
carl <carl.stroemstedt@open-circle.ch>
2023-07-07 16:33:48 +0200

    test: testing

---
+++ spec/requests/stories_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'rails_helper'
+# note: require 'devise' after require 'rspec/rails'
+require 'devise'
+
+
+RSpec.describe "Stories", type: :request do
+  describe 'GET #index' do
+    let(:user) { create(:user) }
+
+    it 'returns a 302 response' do
+      get storytime_path()
+      expect(response).to have_http_status(302)
+    end
+
+    it 'returns a success response' do
+      sign_in user
+      get storytime_path()
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
